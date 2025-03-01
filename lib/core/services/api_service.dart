79ad351c578b042d7243a621f2d2ac844df70d7e
Ronak99 <punase.ronak99@gmail.com>
2025-03-01 12:38:44 +0530

    Setup Api Service

---
+++ lib/core/services/api_service.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+import '../models/company.dart';
+import '../models/company_detail.dart';
+
+class ApiService {
+  final Dio _dio = Dio();
+
+  Future<List<Company>> getCompanies() async {
+    try {
+      final response =
+          await _dio.get('https://eol122duf9sy4de.m.pipedream.net/');
+      final data = response.data['data'] as List;
+      return data.map((json) => Company.fromJson(json)).toList();
+    } catch (e) {
+      throw Exception('Failed to load companies: $e');
+    }
+  }
+
+  Future<CompanyDetail> getCompanyDetail() async {
+    try {
+      final response =
+          await _dio.get('https://eo61q3zd4heiwke.m.pipedream.net/');
+      return CompanyDetail.fromJson(response.data);
+    } catch (e) {
+      throw Exception('Failed to load company detail: $e');
+    }
+  }
+}
